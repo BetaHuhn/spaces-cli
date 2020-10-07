03ba330317fde20e80189435d877079050cb5384
BetaHuhn <51766171+BetaHuhn@users.noreply.github.com>
2020-10-07 11:36:19 +0200

    Custom error message

--- src/index.js
+++ src/index.js
@@ -37,4 +37,10 @@ program
 		runner.outputConfig()
 	})
 
+program.on('command:*', (operands) => {
+	console.error(`error: unknown command '${ operands[0] }'\n`)
+	program.help()
+	process.exitCode = 1
+})
+
 program.parse(process.argv)
